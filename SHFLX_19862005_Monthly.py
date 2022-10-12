2ad16f4941a44755bb0f088337e77a90b83a192b
wiensjus <115589734+wiensjus@users.noreply.github.com>
2022-10-12 11:33:17 -0400

    Add files via upload

---
+++ SHFLX_19862005_Monthly.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 11:38:41 2021
+
+@author: Justin
+"""
+
+import netCDF4 as nc
+import numpy as np
+import numpy.matlib
+import os
+import pickle
+
+data_dir = "/export/data/CESM-LENS/SHFLX/"
+filenames2 = os.listdir(data_dir)
+
+filenames = []
+for i in range(len(filenames2)):
+    if '.nc' in filenames2[i]:
+        filenames.append(filenames2[i])
+
+outfile = open('SHFLX_monthly_filenames', 'wb')
+pickle.dump(filenames, outfile)
+outfile.close()
+
+data = []
+for i in range(len(filenames)):
+    data.append(nc.Dataset(data_dir + "/" + filenames[i]))
+    
+SHFLX = []
+
+for i in range(len(data)):
+    SHFLX.append(np.array(data[i]['SHFLX'][:]))
+    
+SHFLX_19812005 = []
+temp = []
+temp2 = []
+
+for k in range(len(data)):
+    print(k + 1)
+    for i in range(np.shape(SHFLX[0])[2]):
+        for j in range(np.shape(SHFLX[0])[1]):
+            for m in range(12):
+                if k == 0:
+                    temp.append(np.nanmean(SHFLX[k][1632+m:1872+m:12,j,i]))
+                else:
+                    temp.append(np.nanmean(SHFLX[k][792+m:1032+m:12,j,i]))
+            temp2.append(temp)
+            temp = []
+        SHFLX_19812005.append(temp2)
+        temp2 = []
+    
+    if k == 0:
+        outfile = open('SHFLX_19862005_monthly1', 'wb')
+        pickle.dump(SHFLX_19812005, outfile)
+        outfile.close()
+        SHFLX_19812005 = []
+        
+    else:
+        outfile = open('SHFLX_19862005_monthly' + np.str(k + 1), 'wb')
+        pickle.dump(SHFLX_19812005, outfile)
+        outfile.close()
+        SHFLX_19812005 = []
+    
+
+print('done')
